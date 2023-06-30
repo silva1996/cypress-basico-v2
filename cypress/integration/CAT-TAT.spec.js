f6dd35d5b1678494e64704dcd0eeeb2e3ea5b682
silva199630 <marcosj.antonio3@gmail.com>
2023-06-30 10:33:09 -0300

    corrigindo mudanças

--- cypress/integration/CAT-TAT.spec.js
+++ cypress/integration/CAT-TAT.spec.js
@@ -21,7 +21,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('.success').should('be.visible')
     })
 
-    it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function() {
+    it.only('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function() {
         cy.get('#firstName').type('Marcos')
         cy.get('#lastName').type('Junior')
         cy.get('#email').type('marcos@exemplo,com')
